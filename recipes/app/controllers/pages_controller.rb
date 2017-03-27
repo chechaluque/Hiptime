17a0af586ebebb507a24bf254db5f2e63e7a451d
chechaluque <cesarluque2000@gmail.com>
2017-03-27 11:47:27 -0700

    Add delete chefs and assosiated recipes

--- recipes/app/controllers/pages_controller.rb
+++ recipes/app/controllers/pages_controller.rb
@@ -1,11 +1,24 @@
 class PagesController < ApplicationController
+  before_action :chef_params, only: [:show, :destroy]
   def home
   end
   def index
-    @chefs = Chef.all
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
   end
 
   def show
     @chef = Chef.find(params[:id])
+    @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
   end
+
+  def destroy
+    @chef.destroy
+    flash[:danger]= "Chef and all associated recipes have been deleted!"
+    redirect_to pages_path()
+  end
+
+  private
+    def chef_params
+      @chef = Chef.find(params[:id])
+    end
 end
